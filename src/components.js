7cf03c4e845a885e7d2b57c8224b01640404a246
kjohn01 <r01631002@ntu.edu.tw>
2019-01-03 18:12:48 +0800

    basic setup and routes

---
+++ src/components.js
@@ -0,0 +1,7 @@
+import App from './components/App';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+
+export {
+    App, SignIn, SignUp
+};
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Router, Route, browserHistory } from 'react-router';
+import { App, SignIn, SignUp } from './components';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <Router path="/" history={browserHistory}>
+        <Route path="app" component={App} />
+        <Route path="signin" component={SignIn} />
+        <Route path="signup" component={SignUp} />
+    </Router>, document.getElementById('root')
+);
